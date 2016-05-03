70c490e51a6bdb953e2b64b08f134d06ec98159b
Драганчук Алена Сергеевна <ri57s05@awp09.csc.local>
2016-05-03 10:28:51 +0600

    Add status

--- src/sqrt.c
+++ src/sqrt.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include "sqrt.h"
 
-int func(double a, double b, double c){
+int func(double a, double b, double c) {
 	double d;
+	int status;
 	printf("Enter a, b, c: ");
 	d = b * b - 4 * a * c;
 	if (d == 0){
 		*x1 = -b / 2 * a;
+		status = 1;
 	}
 	if (d > 0){
-	*x1 = (-b + sqrt(d)) / 2 * a; 
-	*x2 = (-b - sqrt(d)) / 2 * a;
+		*x1 = (-b + sqrt(d)) / 2 * a; 
+		*x2 = (-b - sqrt(d)) / 2 * a;
+		status = 2;
 	}
-	return 0;
+	if (d < 0){
+		status = 0;
+	}
+	return status;
 }
 	
