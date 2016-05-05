7f7126bfa42ecb47954f31c0d6e38d8c4c2cd3cb
Klepikova Sofya <sofja.klepikowa14@gmail.com>
2016-05-05 16:26:08 +0600

    Add root_test

---
+++ test/test.c
@@ -0,0 +1,24 @@
+
+#include <sqrt.h>
+
+CTEST(sqrt_suite, root_test)
+{
+	//Given
+	const double a = 1.00;
+	const double b = -3.00;
+	const double c = 2.00;
+
+	//When
+	double x1 = 0, x2 = 0;
+	const int status = func(a, b, c, &x1, &x2);
+
+	//Then
+	const double expected_x1 = 1.00;
+	const double expected_x2 = 2.00;
+
+	ASSERT_DBL_NEAR(expected_x1, x1);
+	ASSERT_DBL_NEAR(expected_x2, x2);
+	ASSERT_EQUAL(2, status);
+}
+
+
