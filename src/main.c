04284f0e929c70db04e17692d0ea139e60619661
Драганчук Алена Сергеевна <ri57s05@awp09.csc.local>
2016-05-10 10:46:00 +0600

    Add new tests

--- src/main.c
+++ src/main.c
@@ -7,13 +7,15 @@ int main() {
 	printf("Enter a, b, c: ");
 	scanf("%lf %lf %lf", &a, &b, &c);
 	status = func(a, b, c, &x1, &x2);
-	printf("\nNumber of roots = %d\n", status);
+	printf("Number of roots = %d\n", status);
 	if (status == 0)
 		printf("no solution");
 	if (status == 2)
 		printf("x1 =%.2lf\nx2 =%.2lf\n", x1, x2);
 	if (status == 1)
 		printf("x1 = x2 = %.2lf\n", x1);
+	if (status == -1)
+		printf("It is not quadratic equation");
 	return 0;
 }
 	
--- src/sqrt.c
+++ src/sqrt.c
@@ -6,13 +6,16 @@ int func(double a, double b, double c, double *x1, double *x2) {
 	double d;
 	int status;
 	d = b * b - 4 * a * c;
+	if (a == 0){
+		status = -1;
+	}
 	if (d == 0){
-		*x1 = -b / 2 * a;
+		*x1 = -b / (2 * a);
 		status = 1;
 	}
-	if (d > 0){
-		*x1 = (-b + sqrt(d)) / 2 * a; 
-		*x2 = (-b - sqrt(d)) / 2 * a;
+	if ((d > 0) && (a != 0)){
+		*x1 = (-b + sqrt(d)) / (2 * a); 
+		*x2 = (-b - sqrt(d)) / (2 * a);
 		status = 2;
 	}
 	if (d < 0){
